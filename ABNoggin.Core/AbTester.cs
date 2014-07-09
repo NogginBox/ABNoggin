4736f509e76e6abc8181d39bc7b79779d567f692
Richard Garside <richard.garside@nogginbox.co.uk>
2014-07-09 17:24:56 +0100

    Fleshing out the implementation.

--- ABNoggin.Core/AbTester.cs
+++ ABNoggin.Core/AbTester.cs
@@ -1,18 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 
 
 namespace ABNoggin.Core
 {
     public class ABTester
     {
-	    public T Test<T>(String name, T[] alternatives, String conversion)
+	    public T Test<T>(String experimentName, T[] alternatives, String conversion) where T: struct
 	    {
-		    throw new NotImplementedException();
+		    var experimentQuery = new ExperimentQuery<T>(experimentName);
+
+			var shortCircuitAnswer = experimentQuery.GetAnswerIfExperimentEnded();
+		    if (shortCircuitAnswer != null)
+		    {
+			    return shortCircuitAnswer.Value;
+		    }
+
+		    if (!experimentQuery.Exists())
+		    {
+			    experimentQuery.Create(conversion);
+		    }
+
+		    var choice = experimentQuery.GetAlternativeForUser(UserIdentity);
+		    if (choice == null)
+		    {
+			    choice = PickOne(alternatives);
+			    experimentQuery.SaveChoice(choice.Value, UserIdentity);
+		    }
+
+		    return choice.Value;
 	    }
 
-	    public bool Test(String name, String conversion)
+	    
+
+	    public bool Test(String experimentName, String conversion)
 	    {
-		    throw new NotImplementedException();
+		    return Test(experimentName, new [] {true, false}, conversion);
+	    }
+
+		private static T PickOne<T>(IList<T> alternatives)
+		{
+			var random = new Random(DateTime.Now.Millisecond);
+			var index = random.Next(alternatives.Count - 1);
+			return alternatives[index];
+		}
+
+	    private Guid UserIdentity
+	    {
+			// Todo: How do we work out user identity
+		    get { return Guid.Empty; }
 	    }
     }
 }
---
+++ ABNoggin.Core/ExperimentQuery.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ABNoggin.Core
+{
+	internal class ExperimentQuery<T> where T : struct
+	{
+		private readonly string _experimentName;
+		private readonly Guid _cacheKeyForExperiment;
+		private readonly Dictionary<Guid, String> _cacheForExperiments;
+		private readonly Dictionary<Guid, object> _cacheForUserAlternatives;
+
+		public ExperimentQuery(String experimentName)
+		{
+			_experimentName = experimentName;
+			
+			// Todo: Can I use a string key and make it easier to understand
+			_cacheKeyForExperiment = CreateGuidKeyFromString(experimentName);
+			
+
+			// Todo: Use a switchable and persistent implementation to store experiments
+			_cacheForExperiments = new Dictionary<Guid, String>();
+			_cacheForUserAlternatives = new Dictionary<Guid, object>();
+		}
+
+		public bool Exists()
+		{
+			return _cacheForExperiments.ContainsKey(_cacheKeyForExperiment);
+		}
+
+		public Nullable<T> GetAnswerIfExperimentEnded() 
+		{
+			// Todo: Check cache to see if this has ended
+			return null;
+		}
+
+		private static Guid CreateGuidKeyFromString(String value)
+		{
+			byte[] stringbytes = Encoding.UTF8.GetBytes(value);
+			byte[] hashedBytes = new System.Security.Cryptography
+				.SHA1CryptoServiceProvider()
+				.ComputeHash(stringbytes);
+			Array.Resize(ref hashedBytes, 16);
+			return new Guid(hashedBytes);
+		}
+
+		public void Create(string conversion)
+		{
+			_cacheForExperiments.Add(_cacheKeyForExperiment, conversion);
+		}
+
+		public Nullable<T> GetAlternativeForUser(Guid userIdentity)
+		{
+			var hashKey = CreateGuidKeyFromString(_experimentName + userIdentity);
+			var answer = _cacheForUserAlternatives[hashKey];
+			return (T)answer;
+		}
+
+		public void SaveChoice(T choice, Guid userIdentity)
+		{
+			var hashKey = CreateGuidKeyFromString(_experimentName + userIdentity);
+			_cacheForUserAlternatives.Add(hashKey, choice);
+
+			// Todo: Are we able to query for the number of people taking part
+		}
+	}
+}
